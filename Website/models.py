95e64e98df40af4e112afff33e51e1ac12bbbcf3
vkurakula98 <vkurakula98@gmail.com>
2024-08-03 11:54:59 -0400

    changes

--- Website/models.py
+++ Website/models.py
@@ -1,31 +1,31 @@
-from flask_pymongo import PyMongo
-from flask import Flask
-import bcrypt
-
-app = Flask(__name__)
-app.config.from_object('config.Config')
-mongo = PyMongo(app)
-
-
-class User:
-    @staticmethod
-    def register_user(role, user_id, first_name, last_name, dob, department, email, password):
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        mongo.db.users.insert_one({
-            'role': role,
-            'user_id': user_id,
-            'first_name': first_name,
-            'last_name': last_name,
-            'dob': dob,
-            'department': department,
-            'email': email,
-            'password': hashed_password
-        })
-
-    @staticmethod
-    def get_user_by_email(email):
-        return mongo.db.users.find_one({'email': email})
-
-    @staticmethod
-    def verify_password(stored_password, provided_password):
-        return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password)
+# from flask_pymongo import PyMongo
+# from flask import Flask
+# import bcrypt
+#
+# app = Flask(__name__)
+# app.config.from_object('config.Config')
+# mongo = PyMongo(app)
+#
+#
+# class User:
+#     @staticmethod
+#     def register_user(role, user_id, first_name, last_name, dob, department, email, password):
+#         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+#         mongo.db.users.insert_one({
+#             'role': role,
+#             'user_id': user_id,
+#             'first_name': first_name,
+#             'last_name': last_name,
+#             'dob': dob,
+#             'department': department,
+#             'email': email,
+#             'password': hashed_password
+#         })
+#
+#     @staticmethod
+#     def get_user_by_email(email):
+#         return mongo.db.users.find_one({'email': email})
+#
+#     @staticmethod
+#     def verify_password(stored_password, provided_password):
+#         return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password)
--- Website/views.py
+++ Website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 
-from Website.models import User
+# from Website.models import User
 
 views = Blueprint('views',__name__)
 
@@ -28,28 +28,28 @@ def base():
 def signup():
   return render_template("Signup.html")
 
-@views.route('Register')
-def RegCnf():
-    data = request.get_json()
-    role = data.get('role')
-    user_id = data.get('user_id')
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    dob = data.get('dob')
-    department = data.get('department')
-    email = data.get('email')
-    password = data.get('password')
-    confirm_password = data.get('confirm_password')
-
-    if password != confirm_password:
-        return jsonify({'msg': 'Passwords do not match'}), 400
-
-    existing_user = User.get_user_by_email(email)
-    if existing_user:
-        return jsonify({'msg': 'User already exists'}), 400
-
-    User.register_user(role, user_id, first_name, last_name, dob, department, email, password)
-    return jsonify({'msg': 'User registered successfully'}), 201
+# @views.route('Register')
+# def RegCnf():
+#     data = request.get_json()
+#     role = data.get('role')
+#     user_id = data.get('user_id')
+#     first_name = data.get('first_name')
+#     last_name = data.get('last_name')
+#     dob = data.get('dob')
+#     department = data.get('department')
+#     email = data.get('email')
+#     password = data.get('password')
+#     confirm_password = data.get('confirm_password')
+#
+#     if password != confirm_password:
+#         return jsonify({'msg': 'Passwords do not match'}), 400
+#
+#     existing_user = User.get_user_by_email(email)
+#     if existing_user:
+#         return jsonify({'msg': 'User already exists'}), 400
+#
+#     User.register_user(role, user_id, first_name, last_name, dob, department, email, password)
+#     return jsonify({'msg': 'User registered successfully'}), 201
 
 @views.route('/faq')
 def faq():
